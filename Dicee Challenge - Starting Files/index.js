1b09bae56a93ae36d80ed0705ff9b4cedb68f714
Swapnil <49687612+swapnilmishra25@users.noreply.github.com>
2023-07-27 20:57:00 +0530

    All Files for the project
    This game is developed using HTML, CSS and DOM manipulation using JS. Press refresh for new turn.

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector("img.img1").setAttribute("src","./images/dice"+randomNumber1+".png")
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+document.querySelector("img.img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").textContent = "Player 1 wins";
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").textContent = "Player 2 wins";
+}
+else
+{
+    document.querySelector("h1").textContent = "It's a draw";
+}
